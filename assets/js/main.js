f78ee6ba6f15af993591f08e68fc5ac793fb3d6c
Inga Vaiciakauskaite <i.vaiciakauskaite@gmail.com>
2016-11-01 19:37:19 +0200

    add smooth scroll to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     $(document).on("click", function () {
-        console.log('document click');
         $(".search-field.expanded").removeClass("expanded");
     });
-    $(".fa-search").on("click", function (e) {
+    $(".search-field").on("click", function (e) {
         e.stopPropagation();
-        console.log('search click');
         $(".search-field").addClass("expanded");
+    });
+    $(".toTop").click(function () {
+        $("body").animate({ scrollTop: 0 }, "slow");
     });
 });
